b6f64a58851b22616e537fc07879b3d533557d06
Tyson <thaisenpm2@gmail.com>
2017-08-18 00:37:29 -0600

    Update Form2.vb

--- Loader/WindowsApp1/Form2.vb
+++ Loader/WindowsApp1/Form2.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate("http://localhost/mybb/member.php?action=login")
+    End Sub
+    
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
         Form1.Show()
